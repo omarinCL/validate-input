894d6f36c5d9a3e17af0c1210975a18363ec4ad6
Orlando Marin <orlando.marin@outlook.cl>
2020-02-12 23:43:19 -0300

    CORS

--- src/main/java/cl/option/validateinput/controller/InputValidateController.java
+++ src/main/java/cl/option/validateinput/controller/InputValidateController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,7 @@ import cl.option.validateinput.service.InputValidation;
 @RestController
 @RequestMapping("/api/v1/input")
 @Validated
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET}) 
 public class InputValidateController {
     @Autowired
     private InputValidation service;
